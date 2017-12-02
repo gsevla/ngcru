5cc9eb678e3ceef81938f88945725140cb8dafc0
Gabriel de Sousa Alves <30773847+sousaalves@users.noreply.github.com>
2017-12-02 17:52:40 -0300

    Add files via upload
    estado final 02/10/17

--- testedoteste.py
+++ testedoteste.py
@@ -11,7 +11,20 @@ soup = BeautifulSoup(page.content, 'html.parser')
 ## .tbody retorna todo o conteúdo da tag tbody
 soup_tbody = soup.tbody
 
-## .find_all(tag) retorna o conteúdo da tag td que está dentro da tbody
-listatr = soup_tbody.find_all('td')
-for item in listatr:
+## .find_all(tag) retorna as tags da tag td que está dentro da tbody, mostrando o tipo das tags
+listatd = soup_tbody.find_all('td')
+for item in listatd:
+	print(type(item))
 	print(item.prettify())
+	print("****************")
+
+print("****************************************")
+print("****************************************")
+
+arquivo = open('content.txt', 'a')
+
+## .get_text retorna o conteúdo da tag td e escreve no arquivo
+final = soup.tbody.find_all('td')
+for item in final:
+	print(item.get_text())
+	arquivo.write(item.get_text())
